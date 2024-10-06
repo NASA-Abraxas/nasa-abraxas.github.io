5b332ddf9471bebd3bfb9ad8255cd1606f702261
tteokgook1g <tteokgook1n@naver.com>
2024-10-06 15:17:14 +0900

    update: navigator

--- src/component/ColumnNav.tsx
+++ src/component/ColumnNav.tsx
@@ -1,17 +1,12 @@
-import { useContext } from 'react';
 import styles from './ColumnNav.module.css'
-import { DifficultyContext } from '../context/DifficultyContext';
 export const ColumnNav = () => {
-  const { setDifficulty } = useContext(DifficultyContext);
   return (
     <nav className={styles['nav-container']}>
+      <a href="/page2"><button>Title</button></a>
       <a href="/page4"><button>Chapter 1</button></a>
       <a href="/page8"><button>Chapter 2</button></a>
       <a href="/page10"><button>Chapter 3</button></a>
-
-      <button onClick={() => setDifficulty('junior')}>Junior</button>
-      <button onClick={() => setDifficulty('intermediate')}>Intermediate</button>
-      <button onClick={() => setDifficulty('senior')}>Senior</button>
+      <a href="/page14"><button>Quiz</button></a>
     </nav>
   )
 }
