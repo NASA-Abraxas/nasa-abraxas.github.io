5270f75ff130bbe2349b9e3deba908a996baca5b
1n1tial <dan.yun0821@gmail.com>
2024-10-06 21:52:47 +0900

    fix bgm

--- src/component/ColumnNav.tsx
+++ src/component/ColumnNav.tsx
@@ -1,12 +1,18 @@
 import styles from './ColumnNav.module.css'
+import { AudioContext } from "../context/AudioContext";
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export const ColumnNav = () => {
+  const { setSpace, setTyping } = useContext(AudioContext);
+  const navigate = useNavigate();
   return (
     <nav className={styles['nav-container']}>
-      <a href="/page2"><button>Title</button></a>
-      <a href="/page4"><button>Chapter 1</button></a>
-      <a href="/page8"><button>Chapter 2</button></a>
-      <a href="/page10"><button>Chapter 3</button></a>
-      <a href="/page14"><button>Quiz</button></a>
+      <a onClick={() => {navigate('/page2'); setSpace(true); setTyping(false);}}><button>Title</button></a>
+      <a onClick={() => {navigate('/page4'); setSpace(true);  setTyping(false);}}><button>Chapter 1</button></a>
+      <a onClick={() => {navigate('/page8'); setSpace(true);  setTyping(false);}}><button>Chapter 2</button></a>
+      <a onClick={() => {navigate('/page10'); setSpace(true);  setTyping(false);}}><button>Chapter 3</button></a>
+      <a onClick={() => {navigate('/page14'); setSpace(true);  setTyping(false);}}><button>Quiz</button></a>
     </nav>
   )
 }
--- src/component/NextButton.tsx
+++ src/component/NextButton.tsx
@@ -5,7 +5,7 @@ import { useNavigateNextPage } from "../hook/useNavigateNextPage";
 import styles from './NextButton.module.css';
 
 export const NextButton = ({ duration = 1000, delay = 8000 }: { duration?: number; delay?: number }) => {
-  const { setSpace } = useContext(AudioContext);
+  const { setSpace, setTyping } = useContext(AudioContext);
   const handleNext = useNavigateNextPage();
   const [isClickable, setIsClickable] = useState(false);
   useEffect(() => {
@@ -20,6 +20,7 @@ export const NextButton = ({ duration = 1000, delay = 8000 }: { duration?: numbe
         <button onClick={() => {
           handleNext();
           setSpace(true);
+          setTyping(false);
         }} style={{ visibility: !isClickable ? 'hidden' : undefined }}>NEXT</button>
       </Fade>
     </div>
